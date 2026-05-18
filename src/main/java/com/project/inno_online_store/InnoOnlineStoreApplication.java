4b5cc2c966d3767e927ffd0f7fecac6334efed2b
Howksick <yanzakharevsk@mail.ru>
2026-05-18 14:58:56 +0300

    initial project

---
+++ src/main/java/com/project/inno_online_store/InnoOnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package com.project.inno_online_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnoOnlineStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnoOnlineStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/inno_online_store/InnoOnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.inno_online_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnoOnlineStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
