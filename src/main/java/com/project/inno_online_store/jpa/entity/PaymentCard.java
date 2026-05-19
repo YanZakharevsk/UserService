d2fe04cd4fb36591479da4978ef282c7948d0574
Howksick <yanzakharevsk@mail.ru>
2026-05-19 12:05:44 +0300

    Core entities (User, PaymentCard) have been added

---
+++ src/main/java/com/project/inno_online_store/jpa/entity/PaymentCard.java
@@ -0,0 +1,65 @@
+package com.project.inno_online_store.jpa.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Table(name = "payment_cards")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class PaymentCard extends BaseAuditableEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    @ToString.Exclude
+    private User user;
+
+    @Column(nullable = false, updatable = false)
+    private String number;
+
+    @Column(nullable = false)
+    private String holder;
+
+    @Column(name = "expiration_date", nullable = false)
+    private LocalDate expirationDate;
+
+    @Column(name = "active")
+    private Boolean isActive;
+
+    @Override
+    public final boolean equals(Object o) {
+        if(this == o) return true;
+        if(o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy
+                ?((HibernateProxy) o).getHibernateLazyInitializer()
+                 .getPersistentClass()
+                : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy
+                ?((HibernateProxy) this).getHibernateLazyInitializer()
+                 .getPersistentClass()
+                : this.getClass();
+        if(thisEffectiveClass != oEffectiveClass) return false;
+        User user = (User) o;
+        return getId() != null && Objects.equals(getId(), user.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return this instanceof HibernateProxy
+                ? ((HibernateProxy) this).getHibernateLazyInitializer()
+                  .getPersistentClass()
+                  .hashCode()
+                : getClass().hashCode();
+    }
+}
---
+++ src/main/java/com/project/inno_online_store/jpa/entity/User.java
@@ -0,0 +1,70 @@
+package com.project.inno_online_store.jpa.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+import org.springframework.stereotype.Indexed;
+
+import java.time.LocalDate;
+import java.util.Objects;
+import java.util.Set;
+
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class User extends BaseAuditableEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    private String name;
+
+    private String surname;
+
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+
+    private String email;
+
+    @Column(name = "active")
+    private Boolean isActive;
+
+    @OneToMany(mappedBy = "user", orphanRemoval = true,cascade = CascadeType.ALL)
+    @ToString.Exclude
+    @Size(max = 5)
+    private Set<PaymentCard> paymentCards;
+
+    @Override
+    public final boolean equals(Object o) {
+        if(this == o) return true;
+        if(o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy
+                ?((HibernateProxy) o).getHibernateLazyInitializer()
+                 .getPersistentClass()
+                : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy
+                ?((HibernateProxy) this).getHibernateLazyInitializer()
+                 .getPersistentClass()
+                : this.getClass();
+        if(thisEffectiveClass != oEffectiveClass) return false;
+        User user = (User) o;
+        return getId() != null && Objects.equals(getId(), user.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return this instanceof HibernateProxy
+                ? ((HibernateProxy) this).getHibernateLazyInitializer()
+                  .getPersistentClass()
+                  .hashCode()
+                : getClass().hashCode();
+    }
+}
