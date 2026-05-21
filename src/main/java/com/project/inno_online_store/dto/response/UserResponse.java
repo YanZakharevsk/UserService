5093dda2729b2dd87dc65d4be5b9959dbad808b8
Howksick <yanzakharevsk@mail.ru>
2026-05-21 13:42:25 +0300

    Caching with using Redis has been injected (RedisConfig with cachingManager is added)

--- src/main/java/com/project/inno_online_store/dto/response/UserResponse.java
+++ src/main/java/com/project/inno_online_store/dto/response/UserResponse.java
@@ -2,6 +2,7 @@ package com.project.inno_online_store.dto.response;
 
 import lombok.Data;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.List;
 import java.time.LocalDateTime;
